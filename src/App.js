21dabae4500e61d8ab2a234d148b2f2c66d74242
Seb <sebastien.siddi@gmail.com>
2023-05-03 11:58:01 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Style from "./app.module.css";
+import Chat from "./Chat";
 
 function App() {
+  const [openChat, setOpenChat] = useState(false);
+  const [name1Value, setName1Value] = useState("ChatGTP 1");
+  const [name2Value, setName2Value] = useState("ChatGTP 2");
+  const [promptValue, setPromptValue] = useState(null);
+
+  function openChatWindow() {
+    setOpenChat(true);
+  }
+  function handleName1Value(e) {
+    setName1Value(e.target.value);
+  }
+  function handleName2Value(e) {
+    setName2Value(e.target.value);
+  }
+  function handlePromptValue(e) {
+    setPromptValue(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!openChat ? (
+        <div className={Style.container}>
+          <img src="icon-128.png" className={Style.logo} alt="logo" />
+          <h1 className={Style.title}>ChatGPT Linker</h1>
+          <div className={Style.input_container}>
+            <label className={Style.label}>Change first character name :</label>
+            <input
+              type="text"
+              placeholder="ChatGTP1"
+              id="character1"
+              className={Style.input}
+              value={name1Value}
+              onChange={handleName1Value}
+            />
+            <label className={Style.label}>
+              Change second character name :
+            </label>
+            <input
+              type="text"
+              placeholder="ChatGPT2"
+              id="character2"
+              className={Style.input}
+              value={name2Value}
+              onChange={handleName2Value}
+            />
+            <label className={Style.label}>Enter your prompt :</label>
+            <textarea
+              id="prompt"
+              className={Style.textarea}
+              value={promptValue}
+              onChange={handlePromptValue}
+            />
+          </div>
+          <button
+            id="open-windows"
+            className={Style.button}
+            onClick={openChatWindow}
+          >
+            Start Chat
+          </button>
+        </div>
+      ) : (
+        <div>
+          <Chat
+            setOpenChat={setOpenChat}
+            name1Value={name1Value}
+            name2Value={name2Value}
+            promptValue={promptValue}
+          />
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/Chat.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+import Style from "./chat.module.css";
+import { FaTimes } from "react-icons/fa";
+const { Configuration, OpenAIApi } = require("openai");
+
+const Chat = ({ setOpenChat, name1Value, name2Value, promptValue }) => {
+  const [prompt, setPrompt] = useState(
+    `tu t'appelles ${name1Value}, et tu discutes avec ${name2Value}, tu lui pose une question à ce sujet ${promptValue}`
+  );
+  const [responseChat1, setResponseChat1] = useState([]);
+  const [responseChat2, setResponseChat2] = useState([]);
+  const [responses, setResponses] = useState([]);
+
+  const configurationChat1 = new Configuration({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY_1,
+  });
+  delete configurationChat1.baseOptions.headers["User-Agent"];
+
+  const configurationChat2 = new Configuration({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY_2,
+  });
+  delete configurationChat2.baseOptions.headers["User-Agent"];
+
+  const openaiChat1 = new OpenAIApi(configurationChat1);
+  const openaiChat2 = new OpenAIApi(configurationChat2);
+
+  useEffect(() => {
+    async function generateResponse() {
+      try {
+        if (responseChat1.length === 0) {
+          const completionChat1 = await openaiChat1.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "system", content: prompt }],
+            temperature: 0.5,
+            n: 1,
+          });
+          const data = completionChat1.data.choices[0].message.content;
+          setResponseChat1((prevResponses) => [...prevResponses, data]);
+          setResponses((prevResponses) => [...prevResponses, data]);
+        } else {
+          const completionChat1 = await openaiChat1.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "user",
+                content: responseChat2[responseChat2.length - 1],
+              },
+            ],
+            temperature: 0.5,
+            n: 1,
+          });
+          const data = completionChat1.data.choices[0].message.content;
+          setResponseChat1((prevResponses) => [...prevResponses, data]);
+          setResponses((prevResponses) => [...prevResponses, data]);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    generateResponse();
+  }, [responseChat2]);
+
+  useEffect(() => {
+    async function generateResponse() {
+      try {
+        if (responseChat1.length > 0) {
+          const completionChat2 = await openaiChat2.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "assistant",
+                content: responseChat1[responseChat1.length - 1],
+              },
+            ],
+            temperature: 0.5,
+            n: 1,
+          });
+          const data = completionChat2.data.choices[0].message.content;
+          setResponseChat2((prevResponses) => [...prevResponses, data]);
+          setResponses((prevResponses) => [...prevResponses, data]);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    generateResponse();
+  }, [responseChat1]);
+
+  function closeChatWindows() {
+    setOpenChat(false);
+  }
+
+  useEffect(() => {
+    console.log(responseChat1);
+  }, [responseChat1]);
+
+  useEffect(() => {
+    console.log(responseChat2);
+  }, [responseChat2]);
+
+  return (
+    <div className={Style.container}>
+      <div className={Style.button_container}>
+        <FaTimes className={Style.button} onClick={closeChatWindows} />
+      </div>
+      <div className={Style.chat_container}>
+        <div className={Style.chat_box}>
+          {responses.map((message, index) => (
+            <div
+              key={index}
+              className={
+                index % 2 === 0 ? Style.character1_msg : Style.character2_msg
+              }
+            >
+              {message}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
